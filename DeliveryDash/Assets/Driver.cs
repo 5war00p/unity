4d2e659b24479ee2e5aa432e26f9a6b870c26ca9
5war00p <maddisaiswaroop@gmail.com>
2025-09-07 21:12:21 +0530

    feat: delivery-dash 2d game

---
+++ DeliveryDash/Assets/Driver.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float steerSpeed = 200f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float move = 0f;
+        float steer = 0f;
+
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            steer = 1f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
+    }
+}
